c1c92dd142bde4e4016483b6d6f0df5dcdfd7bce
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-14 19:06:59 -0400

    creating a static file for the activity/disability information

---
+++ assets/activity_data.js
@@ -0,0 +1,93 @@
+const activityData = [
+  {
+    name: 'Autism',
+    id: 1,
+    disabilities: [
+      'Aspergers',
+      'PDD\'s',
+      'Austism Spectrum',
+    ],
+  },
+  {
+    name: 'Hearing Loss/Deafness',
+    id: 2,
+    disabilities: [
+      'Deafness',
+      'Hearing Impaired',
+      'Deaf as a result of illness',
+      'Deaf as a result of age',
+    ],
+  },
+  {
+    name: 'Visual Impairments/Blindness',
+    id: 3,
+    disabilities: [
+      'Color Blindness',
+      'Glaucoma',
+      'Blindness as a result of age',
+      'Cataracts',
+      'Blindness',
+      'Near Sightedness',
+    ],
+  },
+  {
+    name: 'Learning Disabilities',
+    id: 4,
+    disabilities: [
+      'ADD/ADHD',
+      'Dyslexia',
+      'Memory Processing',
+      'Dyscalculia',
+      'Dysgraphia',
+    ],
+  },
+  {
+    name: 'Physical Disabilities',
+    id: 5,
+    disabilities: [
+      'Cerebral Palsy',
+      'Disability from an injury',
+      'Amputation',
+      'Stroke',
+      'Traumatic Brain Injury (TBI)',
+      'Muscular Dystrophy',
+      'Birth Defect',
+    ],
+  },
+  {
+    name: 'Social/Emotional Disabilities',
+    id: 6,
+    disabilities: [
+      'EBD',
+      'Post Traumatic Stress Disorder (PTSD)',
+      'OCC',
+      'Obsessive Compulsive Disorder (OCD)',
+      'Depression',
+    ],
+  },
+  {
+    name: 'Speech and Language',
+    id: 7,
+    disabilities: [
+      'Speech Delay',
+      'Speech Impediment',
+      'Lisp',
+      'Stuttering',
+      'Articulation challenge',
+    ],
+  },
+  {
+    name: 'Cognitive Development',
+    id: 8,
+    disabilities: [
+      'Downs Syndrome',
+      'Fetal Alcohol Syndrome',
+      'Developmental Delay',
+      'Stroke',
+      'Dyspraxia',
+    ],
+  },
+];
+
+
+export default activityData;
