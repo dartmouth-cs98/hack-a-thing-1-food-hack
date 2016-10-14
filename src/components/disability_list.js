38c8dc1e8ccbcfbc8d9b7a42c69df0b891687fc8
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-14 19:06:29 -0400

    creating placeholder components for ActivityView and DisabilityList

---
+++ src/components/disability_list.js
@@ -0,0 +1,15 @@
+// disability_list.js
+import React, { Component } from 'react';
+
+class DisabilityList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div> I am the disability list within a category.</div>
+    );
+  }
+}
+
+export default DisabilityList;
