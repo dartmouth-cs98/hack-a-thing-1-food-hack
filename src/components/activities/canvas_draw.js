bc62e352230074026552a47bbbe0d40d4e2e1de6
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-28 19:27:44 -0400

    initializing the CanvasDraw component

---
+++ src/components/activities/canvas_draw.js
@@ -0,0 +1,14 @@
+// canvas_draw.js
+
+//imports
+import React from 'react';
+
+
+const CanvasDraw = (props) => {
+  console.log(props.params);
+  return(
+    <div> Hi! </div>
+  );
+}
+
+export default CanvasDraw;
