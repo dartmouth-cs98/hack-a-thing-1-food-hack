fe7246d41a6ada2ada0839efd9fbdd5a95d02faa
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-28 19:31:11 -0400

    refactoring the activity data to allow for better URLs

--- src/components/activity_list.js
+++ src/components/activity_list.js
@@ -6,10 +6,11 @@ const ActivityList = (props) => {
   if (!props.categorySelected) {
     return <div className="hidden" />;
   }
-  const listItems = props.categorySelected.activities.map((activity) => {
+  const listItems = Object.keys(props.categorySelected.activities).map((activity_id) => {
+    const activity = props.categorySelected.activities[activity_id];
     return (
       <ActivityListItem
-        key={activity}
+        key={activity.id}
         activitySelected={props.activitySelected}
         categorySelected={props.categorySelected}
         gradeSelected={props.gradeSelected}
